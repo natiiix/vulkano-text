531835643aae4d34d2660c01103d79f35e9d3dc1
Ivo Meixner <natiwastaken@gmail.com>
2023-04-06 04:35:08 +0900

    Update vulkano to 0.32 and winit to 0.28.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,33 @@
-use rusttype::{Font, PositionedGlyph, Scale, Rect, point};
 use rusttype::gpu_cache::Cache;
+use rusttype::{point, Font, PositionedGlyph, Rect, Scale};
 
-use vulkano::buffer::{CpuAccessibleBuffer, BufferUsage};
-use vulkano::command_buffer::{AutoCommandBufferBuilder, SubpassContents, PrimaryAutoCommandBuffer, CopyBufferToImageInfo, RenderPassBeginInfo};
+use vulkano::buffer::TypedBufferAccess;
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
+use vulkano::command_buffer::{
+    AutoCommandBufferBuilder, CopyBufferToImageInfo, PrimaryAutoCommandBuffer, RenderPassBeginInfo,
+    SubpassContents,
+};
+use vulkano::descriptor_set::allocator::StandardDescriptorSetAllocator;
 use vulkano::descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet};
 use vulkano::device::{Device, Queue};
 use vulkano::format::Format;
-use vulkano::render_pass::{Framebuffer, Subpass, FramebufferCreateInfo};
-use vulkano::image::{SwapchainImage, ImmutableImage, ImageCreateFlags, ImageUsage, ImageLayout, ImageDimensions};
 use vulkano::image::view::ImageView;
-use vulkano::pipeline::graphics::{vertex_input::BuffersDefinition,
-    input_assembly::InputAssemblyState, color_blend::ColorBlendState};
-use vulkano::pipeline::graphics::viewport::{Viewport, ViewportState};
-use vulkano::pipeline::{GraphicsPipeline, PipelineBindPoint, Pipeline};
-use vulkano::sampler::{Sampler, Filter, SamplerAddressMode, SamplerCreateInfo};
-use vulkano::swapchain::Swapchain;
-use vulkano::buffer::TypedBufferAccess;
 use vulkano::image::ImageAccess;
+use vulkano::image::{
+    ImageCreateFlags, ImageDimensions, ImageLayout, ImageUsage, ImmutableImage, SwapchainImage,
+};
+use vulkano::memory::allocator::{
+    FreeListAllocator, GenericMemoryAllocator, StandardMemoryAllocator,
+};
+use vulkano::pipeline::graphics::viewport::{Viewport, ViewportState};
+use vulkano::pipeline::graphics::{
+    color_blend::ColorBlendState, input_assembly::InputAssemblyState,
+    vertex_input::BuffersDefinition,
+};
+use vulkano::pipeline::{GraphicsPipeline, Pipeline, PipelineBindPoint};
+use vulkano::render_pass::{Framebuffer, FramebufferCreateInfo, Subpass};
+use vulkano::sampler::{Filter, Sampler, SamplerAddressMode, SamplerCreateInfo};
+use vulkano::swapchain::Swapchain;
 
 use std::sync::Arc;
 
@@ -25,21 +36,21 @@ use bytemuck::{Pod, Zeroable};
 #[repr(C)]
 #[derive(Clone, Copy, Debug, Default, Zeroable, Pod)]
 struct Vertex {
-    position:     [f32; 2],
+    position: [f32; 2],
     tex_position: [f32; 2],
-    color:        [f32; 4]
+    color: [f32; 4],
 }
 vulkano::impl_vertex!(Vertex, position, tex_position, color);
 
 mod vs {
-    vulkano_shaders::shader!{
+    vulkano_shaders::shader! {
         ty: "vertex",
         path: "src/shaders/vertex.glsl",
     }
 }
 
 mod fs {
-    vulkano_shaders::shader!{
+    vulkano_shaders::shader! {
         ty: "fragment",
         path: "src/shaders/fragment.glsl",
     }
@@ -47,34 +58,46 @@ mod fs {
 
 struct TextData {
     glyphs: Vec<PositionedGlyph<'static>>,
-    color:  [f32; 4],
+    color: [f32; 4],
 }
 
 pub struct DrawText {
-    device:             Arc<Device>,
-    queue:              Arc<Queue>,
-    font:               Font<'static>,
-    cache:              Cache<'static>,
+    device: Arc<Device>,
+    queue: Arc<Queue>,
+    font: Font<'static>,
+    cache: Cache<'static>,
     cache_pixel_buffer: Vec<u8>,
-    pipeline:           Arc<GraphicsPipeline>,
-    framebuffers:       Vec<Arc<Framebuffer>>,
-    texts:              Vec<TextData>,
-    viewport:           Viewport,
+    pipeline: Arc<GraphicsPipeline>,
+    framebuffers: Vec<Arc<Framebuffer>>,
+    texts: Vec<TextData>,
+    viewport: Viewport,
+    memory_allocator: Arc<GenericMemoryAllocator<Arc<FreeListAllocator>>>,
+    descriptor_set_allocator: StandardDescriptorSetAllocator,
 }
 
 const CACHE_WIDTH: usize = 1000;
 const CACHE_HEIGHT: usize = 1000;
 
 impl DrawText {
-    pub fn new<W>(device: Arc<Device>, queue: Arc<Queue>, swapchain: Arc<Swapchain<W>>, images: &[Arc<SwapchainImage<W>>]) -> DrawText where W: Send + Sync + 'static + std::fmt::Debug {
+    pub fn new<W>(
+        device: Arc<Device>,
+        queue: Arc<Queue>,
+        swapchain: Arc<Swapchain>,
+        images: &[Arc<SwapchainImage>],
+    ) -> DrawText
+    where
+        W: Send + Sync + 'static + std::fmt::Debug,
+    {
         let font_data = include_bytes!("DejaVuSans.ttf");
         let font = Font::from_bytes(font_data as &[u8]).unwrap();
 
         let vs = vs::load(device.clone()).unwrap();
         let fs = fs::load(device.clone()).unwrap();
 
-        let cache = Cache::builder().dimensions(CACHE_WIDTH as u32, CACHE_HEIGHT as u32).build();
-        let cache_pixel_buffer = vec!(0; CACHE_WIDTH * CACHE_HEIGHT);
+        let cache = Cache::builder()
+            .dimensions(CACHE_WIDTH as u32, CACHE_HEIGHT as u32)
+            .build();
+        let cache_pixel_buffer = vec![0; CACHE_WIDTH * CACHE_HEIGHT];
 
         let render_pass = vulkano::single_pass_renderpass!(
             device.clone(),
@@ -98,25 +121,25 @@ impl DrawText {
             origin: [0.0, 0.0],
             dimensions: [dimensions[0] as f32, dimensions[1] as f32],
             depth_range: 0.0..1.0,
-        }; 
-        let framebuffers = images.iter().map(|image| {
-            let view = ImageView::new_default(image.clone()).unwrap();
-            Framebuffer::new(
-                render_pass.clone(),
-                FramebufferCreateInfo {
-                    attachments: vec![view],
-                    ..Default::default()
-                },
-            )
-            .unwrap()
-        }).collect::<Vec<_>>();
+        };
+        let framebuffers = images
+            .iter()
+            .map(|image| {
+                let view = ImageView::new_default(image.clone()).unwrap();
+                Framebuffer::new(
+                    render_pass.clone(),
+                    FramebufferCreateInfo {
+                        attachments: vec![view],
+                        ..Default::default()
+                    },
+                )
+                .unwrap()
+            })
+            .collect::<Vec<_>>();
 
         let blend_state = ColorBlendState::new(1).blend_alpha();
         let pipeline = GraphicsPipeline::start()
-            .vertex_input_state(
-                BuffersDefinition::new()
-                .vertex::<Vertex>(),
-            )
+            .vertex_input_state(BuffersDefinition::new().vertex::<Vertex>())
             .vertex_shader(vs.entry_point("main").unwrap(), ())
             .input_assembly_state(InputAssemblyState::new())
             .viewport_state(ViewportState::viewport_dynamic_scissor_irrelevant())
@@ -126,6 +149,9 @@ impl DrawText {
             .build(device.clone())
             .unwrap();
 
+        let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
+        let descriptor_set_allocator = StandardDescriptorSetAllocator::new(device.clone());
+
         DrawText {
             device,
             queue,
@@ -134,13 +160,19 @@ impl DrawText {
             cache_pixel_buffer,
             pipeline,
             framebuffers,
-            texts: vec!(),
+            texts: vec![],
             viewport,
+            memory_allocator,
+            descriptor_set_allocator,
         }
     }
 
     pub fn queue_text(&mut self, x: f32, y: f32, size: f32, color: [f32; 4], text: &str) {
-        let glyphs: Vec<PositionedGlyph> = self.font.layout(text, Scale::uniform(size), point(x, y)).map(|x| x.standalone()).collect();
+        let glyphs: Vec<PositionedGlyph> = self
+            .font
+            .layout(text, Scale::uniform(size), point(x, y))
+            .map(|x| x.standalone())
+            .collect();
         for glyph in &glyphs {
             self.cache.queue_glyph(0, glyph.clone());
         }
@@ -150,55 +182,65 @@ impl DrawText {
         });
     }
 
-    pub fn draw_text<'a>(&mut self, command_buffer: &'a mut AutoCommandBufferBuilder<PrimaryAutoCommandBuffer>, image_num: usize) -> &'a mut AutoCommandBufferBuilder<PrimaryAutoCommandBuffer> {
-        let screen_width  = self.framebuffers[image_num].extent()[0];
+    pub fn draw_text<'a>(
+        &mut self,
+        command_buffer: &'a mut AutoCommandBufferBuilder<PrimaryAutoCommandBuffer>,
+        image_num: usize,
+    ) -> &'a mut AutoCommandBufferBuilder<PrimaryAutoCommandBuffer> {
+        let screen_width = self.framebuffers[image_num].extent()[0];
         let screen_height = self.framebuffers[image_num].extent()[1];
         let cache_pixel_buffer = &mut self.cache_pixel_buffer;
         let cache = &mut self.cache;
 
         // update texture cache
-        cache.cache_queued(
-            |rect, src_data| {
+        cache
+            .cache_queued(|rect, src_data| {
                 let width = (rect.max.x - rect.min.x) as usize;
                 let height = (rect.max.y - rect.min.y) as usize;
                 let mut dst_index = rect.min.y as usize * CACHE_WIDTH + rect.min.x as usize;
                 let mut src_index = 0;
 
                 for _ in 0..height {
-                    let dst_slice = &mut cache_pixel_buffer[dst_index..dst_index+width];
-                    let src_slice = &src_data[src_index..src_index+width];
+                    let dst_slice = &mut cache_pixel_buffer[dst_index..dst_index + width];
+                    let src_slice = &src_data[src_index..src_index + width];
                     dst_slice.copy_from_slice(src_slice);
 
                     dst_index += CACHE_WIDTH;
                     src_index += width;
                 }
-            }
-        ).unwrap();
+            })
+            .unwrap();
 
         let buffer = CpuAccessibleBuffer::<[u8]>::from_iter(
-            self.device.clone(),
+            &self.memory_allocator,
             BufferUsage {
                 transfer_src: true,
                 ..BufferUsage::empty()
             },
             false,
-            cache_pixel_buffer.iter().cloned()
-        ).unwrap();
+            cache_pixel_buffer.iter().cloned(),
+        )
+        .unwrap();
 
         let (cache_texture, cache_texture_write) = ImmutableImage::uninitialized(
-            self.device.clone(),
-            ImageDimensions::Dim2d { width: CACHE_WIDTH as u32, height: CACHE_HEIGHT as u32, array_layers: 1 },
+            &self.memory_allocator,
+            ImageDimensions::Dim2d {
+                width: CACHE_WIDTH as u32,
+                height: CACHE_HEIGHT as u32,
+                array_layers: 1,
+            },
             Format::R8_UNORM,
             1,
             ImageUsage {
                 sampled: true,
                 transfer_dst: true,
-                .. ImageUsage::empty()
+                ..ImageUsage::empty()
             },
             ImageCreateFlags::empty(),
             ImageLayout::General,
-            Some(self.queue.queue_family_index())
-        ).unwrap();
+            Some(self.queue.queue_family_index()),
+        )
+        .unwrap();
 
         let sampler = Sampler::new(
             self.device.clone(),
@@ -212,26 +254,28 @@ impl DrawText {
         .unwrap();
 
         let cache_texture_view = ImageView::new_default(cache_texture).unwrap();
-        let layout =
-            self.pipeline.layout().set_layouts().get(0).unwrap();
+        let layout = self.pipeline.layout().set_layouts().get(0).unwrap();
         let set = PersistentDescriptorSet::new(
+            &self.descriptor_set_allocator,
             layout.clone(),
             [WriteDescriptorSet::image_view_sampler(
-                0, cache_texture_view.clone(), sampler.clone(),
+                0,
+                cache_texture_view.clone(),
+                sampler.clone(),
             )],
         )
         .unwrap();
 
         let mut command_buffer = command_buffer
-            .copy_buffer_to_image(
-                CopyBufferToImageInfo::buffer_image(buffer, cache_texture_write)
-            )
+            .copy_buffer_to_image(CopyBufferToImageInfo::buffer_image(
+                buffer,
+                cache_texture_write,
+            ))
             .unwrap()
             .begin_render_pass(
                 RenderPassBeginInfo {
                     clear_values: vec![None],
-                    ..RenderPassBeginInfo::framebuffer(
-                        self.framebuffers[image_num].clone())
+                    ..RenderPassBeginInfo::framebuffer(self.framebuffers[image_num].clone())
                 },
                 SubpassContents::Inline,
             )
@@ -244,63 +288,66 @@ impl DrawText {
                 0,
                 set.clone(),
             );
-            
+
         // draw
         for text in &mut self.texts.drain(..) {
-            let vertices: Vec<Vertex> = text.glyphs.iter().flat_map(|g| {
-                if let Ok(Some((uv_rect, screen_rect))) = cache.rect_for(0, g) {
-                    let gl_rect = Rect {
-                        min: point(
-                            (screen_rect.min.x as f32 / screen_width  as f32 - 0.5) * 2.0,
-                            (screen_rect.min.y as f32 / screen_height as f32 - 0.5) * 2.0
-                        ),
-                        max: point(
-                           (screen_rect.max.x as f32 / screen_width  as f32 - 0.5) * 2.0,
-                           (screen_rect.max.y as f32 / screen_height as f32 - 0.5) * 2.0
-                        )
-                    };
-                    vec!(
-                        Vertex {
-                            position:     [gl_rect.min.x, gl_rect.max.y],
-                            tex_position: [uv_rect.min.x, uv_rect.max.y],
-                            color:        text.color,
-                        },
-                        Vertex {
-                            position:     [gl_rect.min.x, gl_rect.min.y],
-                            tex_position: [uv_rect.min.x, uv_rect.min.y],
-                            color:        text.color,
-                        },
-                        Vertex {
-                            position:     [gl_rect.max.x, gl_rect.min.y],
-                            tex_position: [uv_rect.max.x, uv_rect.min.y],
-                            color:        text.color,
-                        },
-
-                        Vertex {
-                            position:     [gl_rect.max.x, gl_rect.min.y],
-                            tex_position: [uv_rect.max.x, uv_rect.min.y],
-                            color:        text.color,
-                        },
-                        Vertex {
-                            position:     [gl_rect.max.x, gl_rect.max.y],
-                            tex_position: [uv_rect.max.x, uv_rect.max.y],
-                            color:        text.color,
-                        },
-                        Vertex {
-                            position:     [gl_rect.min.x, gl_rect.max.y],
-                            tex_position: [uv_rect.min.x, uv_rect.max.y],
-                            color:        text.color,
-                        },
-                    ).into_iter()
-                }
-                else {
-                    vec!().into_iter()
-                }
-            }).collect();
+            let vertices: Vec<Vertex> = text
+                .glyphs
+                .iter()
+                .flat_map(|g| {
+                    if let Ok(Some((uv_rect, screen_rect))) = cache.rect_for(0, g) {
+                        let gl_rect = Rect {
+                            min: point(
+                                (screen_rect.min.x as f32 / screen_width as f32 - 0.5) * 2.0,
+                                (screen_rect.min.y as f32 / screen_height as f32 - 0.5) * 2.0,
+                            ),
+                            max: point(
+                                (screen_rect.max.x as f32 / screen_width as f32 - 0.5) * 2.0,
+                                (screen_rect.max.y as f32 / screen_height as f32 - 0.5) * 2.0,
+                            ),
+                        };
+                        vec![
+                            Vertex {
+                                position: [gl_rect.min.x, gl_rect.max.y],
+                                tex_position: [uv_rect.min.x, uv_rect.max.y],
+                                color: text.color,
+                            },
+                            Vertex {
+                                position: [gl_rect.min.x, gl_rect.min.y],
+                                tex_position: [uv_rect.min.x, uv_rect.min.y],
+                                color: text.color,
+                            },
+                            Vertex {
+                                position: [gl_rect.max.x, gl_rect.min.y],
+                                tex_position: [uv_rect.max.x, uv_rect.min.y],
+                                color: text.color,
+                            },
+                            Vertex {
+                                position: [gl_rect.max.x, gl_rect.min.y],
+                                tex_position: [uv_rect.max.x, uv_rect.min.y],
+                                color: text.color,
+                            },
+                            Vertex {
+                                position: [gl_rect.max.x, gl_rect.max.y],
+                                tex_position: [uv_rect.max.x, uv_rect.max.y],
+                                color: text.color,
+                            },
+                            Vertex {
+                                position: [gl_rect.min.x, gl_rect.max.y],
+                                tex_position: [uv_rect.min.x, uv_rect.max.y],
+                                color: text.color,
+                            },
+                        ]
+                        .into_iter()
+                    } else {
+                        vec![].into_iter()
+                    }
+                })
+                .collect();
 
             if vertices.len() != 0 {
                 let vertex_buffer = CpuAccessibleBuffer::from_iter(
-                    self.device.clone(),
+                    &self.memory_allocator,
                     BufferUsage {
                         vertex_buffer: true,
                         ..BufferUsage::empty()
